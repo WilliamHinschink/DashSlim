8454fa701da92cdbe0a564c238610a3bffdc99b5
William Hinschink <william@sft.inf.br>
2016-12-15 16:18:16 -0200

    Inserindo relatorios

---
+++ src/main/java/com/sibentek/comercial/controller/RelatorioOrcamentoBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sibentek.comercial.controller;
+
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author william
+ */
+@Named
+@RequestScoped
+public class RelatorioOrcamentoBean implements Serializable{
+    
+}
